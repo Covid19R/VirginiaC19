8f5f1641264a54bdc9af5a8dcbca52ad411a22e4
Morgan DeBusk-Lane <mldebusklane@gmail.com>
2020-04-28 22:43:35 -0400

    Small updates

--- R/utils.R
+++ R/utils.R
@@ -10,29 +10,48 @@
 #' @usage lhs \%>\% rhs
 NULL
 
-url.cases <- "http://www.vdh.virginia.gov/content/uploads/sites/182/2020/03/VDH-COVID-19-PublicUseDataset-Cases.csv"
+url <- "https://www.vdh.virginia.gov/content/uploads/sites/182/2020/03/VDH-COVID-19-PublicUseDataset-Cases.csv"
 
-agg.cases <- readr::read_csv(url.cases)
+# iso.location <- data.frame(
+#   location_code = c("CA-AB", "CA-BC", "CA-MB", "CA-NB", "CA-NL", "CA-NS", "CA-ON", "CA-PE", "CA-QC", "CA-SK", "CA-NT", "CA-NU", "CA-YT"),
+#   location_code_type = "iso_3166_2",
+#   province = c("Alberta", "BC", "Monitoba", "New Brunswick", "NL", "Nova Scotia", "Ontario", "PEI", "Quebec", "Saskatchewan", "NWT", "Repatriated", "Yukon")
+# )
 
-
-# Testing
-library(tidyverse)
-
-url.cases <- "http://www.vdh.virginia.gov/content/uploads/sites/182/2020/03/VDH-COVID-19-PublicUseDataset-Cases.csv"
-
-base.4.21 <- readr::read_csv(url.cases,
-                             col_types =
-                               readr::cols(
-                                 "Report Date" = readr::col_date(format = "%m/%d/%Y")
-                               )
-) %>%
-  janitor::clean_names() %>%
-  dplyr::select(
-    date = report_date,
-    everything()
+read_data <- function() {
+  suppressWarnings(
+    readr::read_csv(
+      url,
+      col_types =
+        readr::cols(
+          "Report Date" = readr::col_date(format = "%m/%d/%Y")
+        )
+    ) %>%
+    janitor::clean_names()
   )
+}
 
-test.4.22 <- base.4.21 %>%
-  mutate(date = as.Date("2020-4-21"))
+clean_data <- function(tbl) {
+  suppressWarnings(
+    tbl %>%
+      dplyr::mutate(
+        location_type = "county",
+        data_type = "cases_total",
+        data_url = "https://www.vdh.virginia.gov/coronavirus/",
+        location_code_type = 'fips_code'
+      ) %>%
+      dplyr::select(
+        report_date,
+        location_code = fips,
+        locality,
+        vdh_health_district,
+        total_cases,
+        hospitalizations,
+        deaths
+      ) %>%
+      dplyr::arrange(
+        report_date
+      ) 
+  )
+}
 
-new.data <- 
