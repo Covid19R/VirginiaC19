87bdd910fa4ef6cd6a22c14012e56f60bff8b9d6
Morgan DeBusk-Lane <mldebusklane@gmail.com>
2020-05-07 22:14:30 -0400

    update demo data

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -6,13 +6,13 @@ setwd(here::here())
 source("./R/refresh_VirginiaC19.R")
 source("./R/utils.R")
 
-VirginiaC19_demo <- refresh_VirginiaC19()
+VirginiaC19 <- refresh_VirginiaC19()
 
 
 #check it
-head(VirginiaC19_demo)
+head(VirginiaC19)
 
 #test with testthat
 
 #deploy data
-usethis::use_data(VirginiaC19_demo, overwrite = TRUE)
+usethis::use_data(VirginiaC19, overwrite = TRUE)
