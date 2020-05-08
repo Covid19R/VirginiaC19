77c810a0ca9af27417096a0df2e230fc1bbcb1be
Morgan DeBusk-Lane <mldebusklane@gmail.com>
2020-05-07 21:56:14 -0400

    new test

---
+++ tests/testthat/test_data.R
@@ -0,0 +1,12 @@
+test_that(desc = "Test the dates", {
+  
+  expect_equal(base::class(VirginiaC19$date) == "Date", TRUE)
+  expect_equal(base::min(covid19swiss$date) == as.Date("2020-03-17"), TRUE)
+})
+
+
+test_that(desc = "Test the structure", {
+  
+  expect_equal(base::ncol(VirginiaC19) == 15, TRUE)
+  
+})
